c40f3fe2ddd31d522f088d5d7e2dabff020a961a
romin <romin@auberginesolutions.com>
2025-01-17 14:50:51 +0530

    RSB-5 refactor: sync active menu with url path

--- src/app/(components)/sidebar/sidebar-menu.tsx
+++ src/app/(components)/sidebar/sidebar-menu.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { MenuList } from "@/menu-list";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import SidebarMenuAccordion from "./menu-accordion";
 import MenuItem from "./menu-item";
 
 const SidebarMenu: React.FC = () => {
+  const pathname = usePathname();
   const [activeMenu, setActiveMenu] = useState<string | null>(null);
   const router = useRouter();
+
+  useEffect(() => {
+    setActiveMenu(pathname);
+  }, [pathname]);
 
   const handleItemClick = (href: string) => {
     setActiveMenu(href);
