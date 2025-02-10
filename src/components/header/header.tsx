230d6a664040cf7a755fbd54a7e519a29aabcadb
romin <romin@auberginesolutions.com>
2025-02-10 19:00:49 +0530

    RSB-11 feat: create roles dropdown and custom UI

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,10 @@
 'use client';
 
-import { Roles } from '@/config/roles';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/core/inputs/dropdown-menu';
-import type { RootState } from '@/store/store';
-import { useSelector } from 'react-redux';
 import { HeaderBreadcrumb } from './header-breadcrumb';
 import { HeaderSmall } from './header-small';
+import { RolesDropdown } from './roles-dropdown';
 
 export const Header: React.FC = () => {
-  const role = useSelector((state: RootState) => state.role.role);
   return (
     <div className='px-5 h-full flex justify-start gap-4 items-center'>
       <div className='lg:hidden'>
@@ -23,19 +12,7 @@ export const Header: React.FC = () => {
       </div>
       <div className='w-full flex justify-between'>
         <HeaderBreadcrumb />
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            {Roles.find((r) => r.id === role)?.name}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <RolesDropdown />
       </div>
     </div>
   );
---
+++ src/components/header/roles-dropdown.tsx
@@ -0,0 +1,46 @@
+import { Roles } from '@/config/roles';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/core/inputs/dropdown-menu';
+import { cn } from '@/lib/utils';
+import { setRole } from '@/store/role-slice';
+import type { RootState } from '@/store/store';
+import { faChevronDown, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDispatch, useSelector } from 'react-redux';
+
+export const RolesDropdown: React.FC = () => {
+  const selectedRole = useSelector((state: RootState) => state.role.role);
+  const dispatch = useDispatch();
+  const handleRoleChange = (roleId: number) => {
+    dispatch(setRole(roleId));
+  };
+
+  return (
+    <div className='flex flex-row-reverse gap-3 items-center text-sm'>
+      <FontAwesomeIcon icon={faUser} size='lg' />
+      <DropdownMenu>
+        <DropdownMenuTrigger className='flex flex-row-reverse gap-3 items-center outline-none'>
+          <FontAwesomeIcon icon={faChevronDown} size='xs' />
+          {Roles.find((role) => role.id === selectedRole)?.name}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='m-2'>
+          {Roles.map((role) => (
+            <DropdownMenuItem
+              key={role.id}
+              onClick={() => handleRoleChange(role.id)}
+              className={cn(
+                role.id === selectedRole && 'bg-gray-200 text-foreground',
+              )}
+            >
+              {role.name}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
