bb243f36250fa58a909eddf2d4e6473fceebf45a
romin <romin@auberginesolutions.com>
2025-02-11 18:33:46 +0530

    RSB-12 refactor: modify login screen ui and validations

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
+import { Label } from '@/core/data-display/label';
+import { Button } from '@/core/inputs/button';
+import { Input } from '@/core/inputs/input';
 import { login } from '@/store/slices/auth-slice';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 
-interface LoginFormInputs {
+interface ILoginProps {
   username: string;
-  password: string;
 }
 
 const LoginPage = () => {
@@ -15,11 +17,11 @@ const LoginPage = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginFormInputs>();
+  } = useForm<ILoginProps>();
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const onSubmit = (data: LoginFormInputs) => {
+  const onSubmit = (data: ILoginProps) => {
     const token = 'dummy-token';
     dispatch(login({ username: data.username, token }));
     router.push('/');
@@ -29,45 +31,30 @@ const LoginPage = () => {
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='bg-white p-6 rounded shadow-md'
+        className='bg-white p-6 rounded shadow-md w-1/4'
       >
         <h1 className='text-2xl font-bold mb-4'>Login</h1>
-
         <div className='mb-4'>
-          <label htmlFor='username' className='block text-sm font-medium'>
-            Username
-          </label>
-          <input
+          <Label htmlFor='username'>Username</Label>
+          <Input
             id='username'
-            {...register('username', { required: 'Username is required' })}
-            className='w-full p-2 border rounded'
+            {...register('username', {
+              setValueAs: (value) => value.trim(),
+              required: 'Username is required',
+              minLength: {
+                value: 3,
+                message: 'Username must be at least 3 characters long',
+              },
+            })}
+            className='h-10'
           />
           {errors.username && (
             <p className='text-red-500 text-sm'>{errors.username.message}</p>
           )}
         </div>
-
-        <div className='mb-4'>
-          <label htmlFor='password' className='block text-sm font-medium'>
-            Password
-          </label>
-          <input
-            id='password'
-            type='password'
-            {...register('password', { required: 'Password is required' })}
-            className='w-full p-2 border rounded'
-          />
-          {errors.password && (
-            <p className='text-red-500 text-sm'>{errors.password.message}</p>
-          )}
-        </div>
-
-        <button
-          type='submit'
-          className='w-full bg-blue-500 text-white p-2 rounded'
-        >
+        <Button type='submit' className='w-full'>
           Login
-        </button>
+        </Button>
       </form>
     </div>
   );
