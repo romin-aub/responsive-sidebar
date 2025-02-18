9ff88fa80dc24bf5b0fed208e5f39d5995b57898
romin <romin@auberginesolutions.com>
2025-02-18 13:08:23 +0530

    RSB-14 refactor: add responsive width in login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -50,7 +50,7 @@ const LoginPage = () => {
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='bg-white p-6 rounded shadow-md w-1/4'
+        className='bg-white p-6 rounded shadow-md w-3/5 md:w-2/5 lg:w-1/3 xl:w-1/4'
       >
         <h1 className='text-2xl font-bold mb-4'>Login</h1>
         <div className='mb-4'>
