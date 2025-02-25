37bc4fb4ff7a170818a4872853d9fe1f3eb32b8f
romin <romin@auberginesolutions.com>
2025-02-25 18:59:27 +0530

    RSB-19 refactor: modify storage util

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,45 +1,50 @@
-type StorageType = 'local' | 'cookie';
+type IStorageType = 'local' | 'cookie';
+
+interface IStorageHandler {
+  set: (key: string, value: string) => void;
+  get: (key: string) => string | null;
+  remove: (key: string) => void;
+}
+
+const LocalStorageHandler: IStorageHandler = {
+  set: (key: string, value: string) => localStorage.setItem(key, value),
+  get: (key: string) => localStorage.getItem(key),
+  remove: (key: string) => localStorage.removeItem(key),
+};
+
+const CookieStorageHandler: IStorageHandler = {
+  set: (key: string, value: string) => {
+    document.cookie = `${key}=${value}; path=/; HTTPOnly; Secure; SameSite=strict`;
+  },
+  get: (key: string) => {
+    return (
+      document.cookie
+        .split('; ')
+        .find((row) => row.startsWith(`${key}=`))
+        ?.split('=')[1] || null
+    );
+  },
+  remove: (key: string) => {
+    document.cookie = `${key}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+  },
+};
 
 class Storage {
-  private readonly storageHandler: {
-    set: (key: string, value: string) => void;
-    get: (key: string) => string | null;
-    remove: (key: string) => void;
-  };
+  private readonly storageHandler: IStorageHandler;
 
-  constructor(type: StorageType = 'local') {
-    this.storageHandler = this.initStorageHandler(type);
+  constructor(type: IStorageType = 'local') {
+    this.storageHandler = this.getStorageHandler(type);
   }
 
-  private initStorageHandler(type: StorageType) {
-    if (type === 'local') {
-      return {
-        set: (key: string, value: string) => localStorage.setItem(key, value),
-        get: (key: string) => localStorage.getItem(key),
-        remove: (key: string) => localStorage.removeItem(key),
-      };
+  private getStorageHandler(type: IStorageType) {
+    switch (type) {
+      case 'local':
+        return LocalStorageHandler;
+      case 'cookie':
+        return CookieStorageHandler;
+      default:
+        throw new Error('Invalid storage type');
     }
-    if (type === 'cookie') {
-      return {
-        set: (key: string, value: string) => {
-          document.cookie = `${key}=${value}; path=/; HTTPOnly; Secure; SameSite=strict`;
-        },
-        get: (key: string) => {
-          const cookies = document.cookie.split(';');
-          for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.startsWith(`${key}=`)) {
-              return cookie.substring(key.length + 1);
-            }
-          }
-          return null;
-        },
-        remove: (key: string) => {
-          document.cookie = `${key}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
-        },
-      };
-    }
-    throw new Error('Invalid storage type');
   }
 
   set(key: string, value: string) {
