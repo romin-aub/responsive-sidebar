a1d52e38f0aad8f5dec1aa7da14534b2ab9db8f0
romin <romin@auberginesolutions.com>
2025-03-12 18:38:01 +0530

    RSB-2 feat: optimize the middleware to protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-const isPublicRoute = createRouteMatcher(['/login', '/sign-up']);
+
+const isPublicRoute = createRouteMatcher(['/login(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-  if (!userId && !isPublicRoute(req)) {
-    return NextResponse.redirect(new URL('/login', req.url));
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
-  if (userId && isPublicRoute(req)) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-  return NextResponse.next();
 });
 
 export const config = {
